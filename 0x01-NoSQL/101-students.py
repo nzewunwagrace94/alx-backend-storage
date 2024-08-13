58472874090b9439a7759be4599ed87269056e94
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2024-08-13 18:53:21 +0100

    Top students

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Write a Python function that returns all students sorted by average score:
+
+Prototype: def top_students(mongo_collection):
+mongo_collection will be the pymongo collection object
+The top must be ordered
+The average score must be part of each item returns with key = averageScore
+"""
+
+
+def top_students(mongo_collection):
+    """returns all students sorted by average score"""
+    return mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name", "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averagecore": -1}}
+    ])
